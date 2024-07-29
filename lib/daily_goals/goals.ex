69df0d97b56a9238ec3b7dbc3a7e3c7f030b3d19
Nicolas Dabreo <nicolas@megalixir.co.uk>
2024-07-29 09:35:22 +0100

    :memo: Adding a little bit of documentation to the main file

--- lib/daily_goals/goals.ex
+++ lib/daily_goals/goals.ex
@@ -6,8 +6,10 @@ defmodule DailyGoals.Goals do
   def subscribe, do: Phoenix.PubSub.subscribe(DailyGoals.PubSub, topic())
   def broadcast(event), do: Phoenix.PubSub.broadcast(DailyGoals.PubSub, topic(), event)
 
+  @doc "Sets up an ETS table for us to use, this is run on Application startup"
   def init_table, do: ETS.new(:goals, [:set, :public, :named_table, read_concurrency: true])
 
+  @doc "Creates a goal tuple under the goal_id, also stores the date for use in match specs"
   def create_goal(persona_id, params, date \\ Date.utc_today()) do
     goal_id = unique_id()
 
@@ -35,6 +37,7 @@ defmodule DailyGoals.Goals do
     goal
   end
 
+  @doc "Retrieves all goals for the given date, using the match spec to match the dates"
   def list_goals(date) do
     match_spec = [{{:_, date, :_}, [], [:"$_"]}]
 
@@ -44,14 +47,23 @@ defmodule DailyGoals.Goals do
     |> Enum.reverse()
   end
 
+  @doc "Retrieves a goal from the table by its ID"
   def get_goal(id) do
     ETS.lookup(:goals, id)
   end
 
+  @doc "Updates the progress of a goal through its numerical steps"
   def update_goal_progress(%Goal{steps: steps} = goal, progress) when steps == progress do
     update_goal(%{goal | progress: progress, completed_at: DateTime.utc_now()})
   end
 
+  @doc """
+  Toggles the completion state of a Goal by changing its completed at to the current date,
+  or by nilifying it.
+
+  This has some side-effects for numerical goals to improve UX, a completed goal will have its
+  progress maxed, an incomplete goal will have its progress reverted to one number before max.
+  """
   def update_goal_progress(%Goal{steps: steps} = goal, progress) when not is_nil(steps) do
     update_goal(%{goal | progress: progress})
   end
@@ -102,6 +114,9 @@ defmodule DailyGoals.Goals do
     %{"goal_text" => "Call a friend", "steps" => ""}
   ]
 
+  @doc """
+  Generates some random seed data for testing
+  """
   def generate_seed_data(persona_id, date) do
     for _i <- 1..Enum.random(2..10), do: create_goal(persona_id, Enum.random(@seed_goals), date)
   end
